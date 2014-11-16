df6bf36a3df1fb8cb1e395b4b4868992cbd68ee4
Lowell Bander <lowellbander@gmail.com>
2014-11-16 09:45:04 -0500

    runs locally

---
+++ app/scripts/app.js
@@ -0,0 +1,33 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name accidontApp
+ * @description
+ * # accidontApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('accidontApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
